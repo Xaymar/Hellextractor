5c4d2ae9215cdb1c6367f697c45bda6c90849fb5
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2024-02-26 13:49:18 +0100

    Add recursive decoding to VGMStream script

--- extra/vgmstream/decode.bat
+++ extra/vgmstream/decode.bat
@@ -1,1 +1,57 @@
-"%~dp0\vgmstream-cli.exe" -o "%~dpn1.wav" "%~1"
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET "SCRIPT=%~0"
+SET "SCRIPTROOT=%~dp0"
+
+:FILELOOP
+	CALL :INDEX "%~1"
+	SHIFT /1
+	IF "%~n1" == "" (
+		GOTO :EOF
+	)
+GOTO :FILELOOP
+
+:CHECK
+	IF "%~nx1"=="$RECYCLE.BIN" (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"=="System Volume Information" (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"==".." (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"=="." (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"=="" (
+		ECHO. > NUL
+	) ELSE IF NOT "%~x1"==".wem" (
+		ECHO. > NUL
+	) ELSE (
+		CALL :INDEX "%~1"
+	)
+EXIT /B 0
+
+:INDEX
+	SET "IS_DIR=0"
+	ECHO.%~a1 | find "d" >NUL 2>NUL && (
+		set "IS_DIR=1"
+	)
+	if "!IS_DIR!"=="1" (
+		FOR %%i IN ("%~1\*") DO (
+			CALL :CHECK "%%~i"
+		)
+		FOR /D %%i IN ("%~1\*") DO (
+			CALL :CHECK "%%~i"
+		)
+	) ELSE (
+		CALL :TRANSCODE "%~1"
+	)
+EXIT /B 0
+
+:TRANSCODE
+	ECHO %~dpn1
+	"%~dp0\vgmstream-cli.exe" -o "%~dpn1.wav" "%~1"
+	if ERRORLEVEL 1 (
+		PAUSE
+	)
+EXIT /B 0
+
