965ee0f71f5013afa050365ec750cc8a93036028
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2024-02-26 13:44:42 +0100

    Add QuickBMS and VGMStream scripts
    The QuickBMS script is based on h3x3r's work, but exports all data all the time. For types it does not understand, it will simply dump the main data, stream data and gpu_resources data into separate files. For types it does understand it will merge as needed.

---
+++ extra/QuickBMS/extract.bat
@@ -0,0 +1,56 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET "SCRIPT=%~0"
+SET "SCRIPTROOT=%~dp0"
+
+:FILELOOP
+	CALL :INDEX "%~1"
+	SHIFT /1
+	IF "%~n1" == "" (
+		GOTO :EOF
+	)
+GOTO :FILELOOP
+
+:CHECK
+	IF "%~nx1"=="$RECYCLE.BIN" (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"=="System Volume Information" (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"==".." (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"=="." (
+		ECHO. > NUL
+	) ELSE IF "%~nx1"=="" (
+		ECHO. > NUL
+	) ELSE IF NOT "%~x1"=="" (
+		ECHO. > NUL
+	) ELSE (
+		CALL :INDEX "%~1"
+	)
+EXIT /B 0
+
+:INDEX
+	SET "IS_DIR=0"
+	ECHO.%~a1 | find "d" >NUL 2>NUL && (
+		set "IS_DIR=1"
+	)
+	if "!IS_DIR!"=="1" (
+		FOR %%i IN ("%~1\*") DO (
+			CALL :CHECK "%%~i"
+		)
+		FOR /D %%i IN ("%~1\*") DO (
+			CALL :CHECK "%%~i"
+		)
+	) ELSE (
+		CALL :TRANSCODE "%~1"
+	)
+EXIT /B 0
+
+:TRANSCODE
+	ECHO %~dpn1
+	"%SCRIPTROOT%\quickbms_4gb_files.exe" -o -. -e "%SCRIPTROOT%\datafile.bms" "%~1" D:\output
+	if ERRORLEVEL 1 (
+		PAUSE
+	)
+EXIT /B 0
---
+++ extra/vgmstream/decode.bat
@@ -0,0 +1,1 @@
+"%~dp0\vgmstream-cli.exe" -o "%~dpn1.wav" "%~1"
